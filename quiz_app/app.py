ef03462947bdb528ccf7c24118f717d63af9c882
Santiago Perez <santhiagoperezpino@gmail.com>
2025-04-26 19:10:48 -0500

    env, index.html, quiestions.json

--- quiz_app/app.py
+++ quiz_app/app.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/') # Define la ruta para la página de inicio(raíz)
+def index():
+    return render_template('index.html') # Renderiza el template index.html
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
